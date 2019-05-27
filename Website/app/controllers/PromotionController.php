4306ebc81bda3158414a05c7386bd6d55828ea1d
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2019-05-27 17:03:48 +0530

    Report, Districtwise Search

--- Website/app/controllers/PromotionController.php
+++ Website/app/controllers/PromotionController.php
@@ -11,36 +11,33 @@ class PromotionController extends Controller {
         $this->view->setLayout('default');
     }
 
+    /**************************************
+     * The index of the promotion page... including Search and districtwise finding
+     ***************************************/
+
     public function indexAction($district = "") {
         $districtList = array('districts'=>array("Colombo","Kandy","Kurunegala","Gampaha","Matara"),
                 'towns'=>array(
                     array("Colombo","Moratuwa","Dehiwala"),
                     array("Kandy","Moratuwa","Dehiwala"),
-                    array("Kurunegala","Moratuwa","Dehiwala"),
+                    array("Kurunegala","Polgahawela","Kuliyapitiya"),
                     array("Gampaha","Negombo"),
-                    array("Matara","Negombo")   
+                    array("Matara","Weligama")   
                 ));
-        // $posted_value = ['promoter' =>'off','catagory'=>'off','search' =>''];
         $promos = $this->PromotionModel->find(['conditions'=>'end_date > ?','bind'=>[currentDate()],'order' => "start_date DESC"]);
         $this->view->searchResults = array($promos,$districtList);
-        // dnd($this->view->searchResults[1]);
-        // dnd($district);
-        // dnd($_POST['town']);
+        
         if($_POST) {
-            // dnd($_POST['town']);
-            // $posted_values = mergeArray($posted_value,posted_values($_POST));
             $p = new Promotion();
-            // dnd($p->Search($_POST));
             $this->view->searchResults[0] = $p->Search($_POST);
             
         }
         elseif($district!=""){
-            // dnd($district);  
+            
+
             $p = new Promotion();
             $params= array("search"=>$district);
-            // dnd($p->Search($params));
             $this->view->searchResults[0] = $p->Search($params);
-            // dnd($district);
             
         }
         
--- Website/app/views/promotion/index.php
+++ Website/app/views/promotion/index.php
@@ -14,6 +14,7 @@
 <?php $this->setSiteTitle("වාසි.lk"); ?>
 
 <?php $this->start('head'); ?>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">  
     <link rel="stylesheet" href="<?=PROOT?>css/promotion_card.css">
 
     
@@ -43,130 +44,116 @@
     </header>
 
     <section class="about-area gray-bg section-padding">
-        <div class="container search">
-            <!-- <div class="single-sidebar-widget widget_search">
-                <form action="#">
-                    <input type="text" name="search" id="search" placeholder="Search Here...">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-            </div> -->
-            <div class="single-sidebar-widget widget_search">
-                <form method= "post" style="border : none;">
-                    <!-- <div class="dropdown pull-right">
-                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                            <span class="glyphicon glyphicon-align-justify" aria-hidden="true"></span>
-                        </button> -->
-                        <!-- <ul class="dropdown-menu" aria-labelledby="dropdownMenu1" method="post">
-                            <div class="input-group">
-                                <input name = "promoter" id="promoter" type="checkbox" aria-label="...">
-                                <label for="promoter">Promoter</label>
-                            </div>
-                            <li role="separator" class="divider"></li>
-                            <div class="input-group">
-                                <span class="input-group-addon">
-                                    <input name = "catagory" id="catagory" type="checkbox" aria-label="...">
-                                </span><label for="catagory">Catagory</label>
-                            </div>
-                        </ul> 
-                     </div> -->
-                
-                    <div class="input-group pull-right">
-                        <input type="text" class="form-control" id="search" name="search"  placeholder="Search">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
-                        </span>
-                    </div>
-                </form>
-            </div> 
+    
+        <div class="container search" id="inc-len">
             
-        </div>
-        <div class= "panel-body">
-            <form id="district" action="<?=PROOT?>promotion/index/" method= "post" style="border : none; ">    
-                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">
-                Select Location     
-                <span class="glyphicon glyphicon-search"></span></button>
-
-                <!-- Modal -->
-                <div class="modal fade  " id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog " role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Select a City</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
+            <div class = "col-sm-5">
+                <form id="district" action="<?=PROOT?>promotion/index/" method= "post" style="border : none; ">    
                             
-                            <div class=".p-3">
-                                
-                                <div class="dropdown">
-                                    <div class="col-sm-6">
-                                        <ul class="nav nav-pills nav-stacked">
-                                            <?php
-                                            $midlength = (int)(sizeof($districtList["districts"])/2)+1;
-                                            $townlist = $districtList["towns"];
-                                            // dnd($midlength);
-                                            for($i=0;$i<$midlength;$i++){ 
-                                                $ln = sizeof($townlist[$i]);
-                                                echo("<li class='dropdown'>
-                                                    <a class='dropdown-toggle' data-toggle='dropdown' href='#'  >".
-                                                    
-                                                        $districtList["districts"][$i]."
-                                                        <b class='caret'></b>
-                                                    </a>
-                                                    <ul class='dropdown-menu' >");
-                                                    for($j=0;$j<$ln;$j++){      
-                                                        // echo "<input type='hidden' name='town' value='".$townlist[$i][$j]."'>";  
-                                                        echo"<li ><a href='".$townlist[$i][$j]."'><p>".$townlist[$i][$j]."<p></a></li>";   
-                                                    }
-                                                echo("</ul>
-                                                </li>");
-                                            }
-                                            ?>
-                                        </ul>
-                                
-                                    </div> 
-                                    <div class="col-sm-6">
-                                        <ul class="nav nav-pills nav-stacked">
-                                            <?php
-                                            $midlength = sizeof($districtList["districts"])/2+1;
-                                            $length = sizeof($districtList["districts"]);
-                                            $townlist = $districtList["towns"];
-                                            for($i=$midlength;$i<$length;$i++){ 
-                                                $ln = sizeof($townlist[$i]);
-                                                echo("<li class='dropdown'>
-                                                    <a class='dropdown-toggle' data-toggle='dropdown' href='#'  >".
-                                                    
-                                                        $districtList["districts"][$i]."
-                                                        <b class='caret'></b>
-                                                    </a>
-                                                    <ul class='dropdown-menu' >");
-                                                    for($j=0;$j<$ln;$j++){      
-                                                        // echo "<input type='hidden' name='town' value='".$townlist[$i][$j]."'>";  
-                                                        echo"<li ><a href='".$townlist[$i][$j]."'><p>".$townlist[$i][$j]."<p></a></li>";   
-                                                    }
-                                                echo("</ul>
-                                                </li>");
-                                            }
-                                            ?>
-                                        </ul>
-                                        
-                                    </div> 
+                    <button type="button" id="locationbtn" class="btn btn-primary  pull-left" data-toggle="modal" data-target="#exampleModal">
+                        Select Location
+                        <i class="material-icons" style="font-size:18px">place</i>
+                    </button>
+                            
+                            
+                            
+                    <div class="modal fade  " id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog " role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Select a City</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
                                 </div>
                                 
-                            </div>    
-                            
-                            <div class="modal-footer">
-                            <div class="col-sm-6"></div>
-                            <div class="col-sm-6">
-                                <button type="button" id="modal-close" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        </div>
+                                <div class=".p-3">
+                                    
+                                    <div class="dropdown">
+                                        <div class="col-sm-6">
+                                            <ul class="nav nav-pills nav-stacked">
+                                                <?php
+                                                    $midlength = (int)(sizeof($districtList["districts"])/2)+1;
+                                                    $townlist = $districtList["towns"];
+                                                    // dnd($midlength);
+                                                    for($i=0;$i<$midlength;$i++){ 
+                                                        $ln = sizeof($townlist[$i]);
+                                                        echo("<li class='dropdown'>
+                                                            <a class='dropdown-toggle' data-toggle='dropdown' href=''  >".
+                                                            
+                                                                $districtList["districts"][$i]."
+                                                                <b class='caret'></b>
+                                                            </a>
+                                                            <ul class='dropdown-menu' >");
+                                                            for($j=0;$j<$ln;$j++){      
+                                                                echo "<input type='hidden' name='town' value='".$townlist[$i][$j]."'>";  
+                                                                echo"<li ><a href=''><p>".$townlist[$i][$j]."<p></a></li>";   
+                                                            }
+                                                        echo("</ul>
+                                                        </li>");
+                                                    }
+                                                 ?>
+                                            </ul>
+                                        
+                                        </div> 
+                                        <div class="col-sm-6">
+                                            <ul class="nav nav-pills nav-stacked">
+                                                <?php
+                                                    $midlength = sizeof($districtList["districts"])/2+1;
+                                                    $length = sizeof($districtList["districts"]);
+                                                    $townlist = $districtList["towns"];
+                                                    for($i=$midlength;$i<$length;$i++){ 
+                                                        $ln = sizeof($townlist[$i]);
+                                                        echo("<li class='dropdown'>
+                                                            <a class='dropdown-toggle' data-toggle='dropdown' href='#'  >".
+                                                            
+                                                                $districtList["districts"][$i]."
+                                                                <b class='caret'></b>
+                                                            </a>
+                                                            <ul class='dropdown-menu' >");
+                                                            for($j=0;$j<$ln;$j++){      
+                                                                // echo "<input type='hidden' name='town' value='".$townlist[$i][$j]."'>";  
+                                                                echo"<li ><a href='".$townlist[$i][$j]."'><p>".$townlist[$i][$j]."<p></a></li>";   
+                                                            }
+                                                        echo("</ul>
+                                                        </li>");
+                                                    }
+                                                ?>
+                                            </ul>
+                                        
+                                        </div> 
+                                    </div>
+                                    
+                                </div>    
+                                
+                                <div class="modal-footer">
+                                    <div class="col-sm-6"></div>
+                                    <div class="col-sm-6">
+                                        <button type="button" id="modal-close" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
+
+
+            <div class = " col-sm-7 ">
+                <div class="single-sidebar-widget widget_search ">
+                    <form method= "post" style="border : none;">                    
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="search" name="search"  placeholder="Search">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+                            </span>
+                        </div>
+                    </form>
+                </div> 
+            </div> 
         </div>
+    </div>
+        
         <div class="panel-body">
             <div class="container">
                 <div class="row">
