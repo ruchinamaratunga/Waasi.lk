110b1ffacd5c485dd189299177f3417a7ff4ecf1
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2019-05-29 19:06:30 +0530

    json file used to store city list for promotions

--- Website/app/controllers/PromotionController.php
+++ Website/app/controllers/PromotionController.php
@@ -16,14 +16,23 @@ class PromotionController extends Controller {
      ***************************************/
 
     public function indexAction($district = "") {
-        $districtList = array('districts'=>array("Colombo","Kandy","Kurunegala","Gampaha","Matara"),
-                'towns'=>array(
-                    array("Colombo","Moratuwa","Dehiwala"),
-                    array("Kandy","Moratuwa","Dehiwala"),
-                    array("Kurunegala","Polgahawela","Kuliyapitiya"),
-                    array("Gampaha","Negombo"),
-                    array("Matara","Weligama")   
-                ));
+
+        $acl_file = file_get_contents(ROOT . DS . 'app' . DS . 'location.json');
+        $acl = json_decode($acl_file,true);
+        $districtList = array_chunk($acl, ceil(count($acl) / 2));
+
+        // list($array1, $array2) = array_chunk($array, ceil(count($array) / 2));
+        // dnd($districtList);
+        // dnd($acl);
+
+        // $districtList = array('districts'=>array("Colombo","Kandy","Kurunegala","Gampaha","Matara"),
+        //         'towns'=>array(
+        //             array("Colombo","Moratuwa","Dehiwala"),
+        //             array("Kandy","Moratuwa","Dehiwala"),
+        //             array("Kurunegala","Polgahawela","Kuliyapitiya"),
+        //             array("Gampaha","Negombo"),
+        //             array("Matara","Weligama")   
+        //         ));
         $promos = $this->PromotionModel->find(['conditions'=>['end_date > ?','state = ?'],'bind'=>[currentDate(),'Approved'],'order' => "start_date DESC"]);
         $this->view->searchResults = array($promos,$districtList);
 
